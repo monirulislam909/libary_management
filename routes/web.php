9d890875e9a0e968e26c0aabd6f491a29d6d91bf
monirulislam909 <monirulislaminfo909@gmail.com>
2025-08-01 22:29:01 +0600

    create route and config dasboard template

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
+
+
+Route::resource('/student', StudentController::class);
+Route::resource('/book', BookController::class);
